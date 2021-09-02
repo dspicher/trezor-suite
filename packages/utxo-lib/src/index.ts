b7e1ac334554d66f57de9fbf7a5b08d49fcfa80c
Szymon Lesisz <sz.lesisz@gmail.com>
2021-09-02 13:50:50 +0200

    feat(utxo-lib): add transaction module
    upstream: https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/ts_src/transaction.ts
    fork: https://github.com/trezor/trezor-utxo-lib/blob/trezor/src/transaction.js
    
    differences:
    - `TransactionBase` is an abstract class. Methods like fromBuffer/toBuffer are enhanced by network type implementation.
    - `tx.outs[x].amount` as string.
    - removed unused methods: addInput, addOutput, clone, hashForSignature, hashForWitnessV0, setInputScript, setWitness.
    - added new fields: network, type, timestamp, expiry.
    - added new methods: getExtraData, getSpecificData.

--- packages/utxo-lib/src/index.ts
+++ packages/utxo-lib/src/index.ts
@@ -4,6 +4,8 @@ import * as payments from './payments';
 import * as script from './script';
 import * as networks from './networks';
 
+export { Transaction } from './transaction';
+
 export { address, crypto, payments, script, networks };
 
 export type { Network } from './networks';
